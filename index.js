f74257e5e2c82d0f7d23c7c4ed6ad235f578e803
Jakaria030 <gulamjakaria99@gmail.com>
2024-12-06 21:26:11 +0600

    Read single visa functionality added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -42,6 +42,14 @@ async function run() {
     app.get('/visas', async(req, res) => {
       const cursor = visaCollections.find();
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // read single visa
+    app.get('/visas/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await visaCollections.findOne(query);
       res.send(result);
     });
 
