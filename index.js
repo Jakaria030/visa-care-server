dc23d7497fe8469cdcbc1e4b101e3f9b3d58be12
Jakaria030 <gulamjakaria99@gmail.com>
2024-12-07 13:31:44 +0600

    Signle visa delete api added

--- index.js
+++ index.js
@@ -62,9 +62,16 @@ async function run() {
       res.send(result);
     });
 
+    // delete single visa
+    app.delete('/visas/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await visaCollections.deleteOne(query);
+      res.send(result);
+    });
+
 
     // user application visa api's here
-
     // create application
     app.post('/applications', async(req, res) => {
       const application = req.body;
